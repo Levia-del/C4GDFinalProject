1b1659154c6eb7ea0eb76a709be67e7e046552cb
Levia-del <levlola579@gmail.com>
2026-07-13 15:17:40 -0400

    Transition Done

---
+++ C4GDFinalProjectU/Assets/Scripts/General/LevelsManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelsManager : MonoBehaviour
+{
+    public static LevelsManager instance;
+
+    private string[] levels = {"Questions", "Dodge"};
+    public int currLvl = -1;
+    public int nextLvl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        currLvl = generateRadnLvl();
+        nextLvl = generateRadnLvl();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+
+    int generateRadnLvl()
+    {
+        int lvl = Random.Range(0, levels.Length);
+        while(lvl==currLvl)
+        {
+            lvl = Random.Range(0, levels.Length);
+        }
+        return lvl;
+    }
+
+    public void LevelComplete()
+    {
+        SceneManager.LoadScene(levels[currLvl]);
+        currLvl = nextLvl;
+        nextLvl = generateRadnLvl();
+    }
+}
---
+++ C4GDFinalProjectU/Assets/Scripts/Transition/TransitionCanvas.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TransitionCanvas : MonoBehaviour
+{
+    public LevelsManager LvlManager;
+    public TMP_Text instTXT;
+    public float waitTime = 5f;
+
+    private float wait = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LvlManager = FindFirstObjectByType<LevelsManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wait > 0)
+        {
+            wait -= Time.deltaTime;
+        }
+        else
+        {
+            string mess="";
+            if (LvlManager.currLvl == 0)
+            {
+                mess = "Answer correctly with A-D or die!";
+            }
+            else if (LvlManager.currLvl == 1)
+            {
+                mess = "Dodge with A-D or die!";
+            }
+            instTXT.text = mess;
+        }
+
+        if(wait <=0)
+        {
+            if(waitTime>0)
+            {
+                waitTime -= Time.deltaTime;
+            }
+            else
+            {
+                LvlManager.LevelComplete();
+            }
+        }
+    }
+}
